4674bc877f20e3cb79b5d0aad2de76901d5cea13
dophuochung2428 <dophuochung2428@gmail.com>
2025-03-11 14:27:04 +0700

    update

---
+++ src/main/java/com/example/movie_theater/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.example.movie_theater.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().info(new Info()
+                .title("API-service document")
+                .version("v1.0.0")
+                .description("description")
+                .license(new License()
+                        .name("MIT License")
+                        .url("https://opensource.org/licenses/MIT")))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList("bearerAuth")));
+    }
+}
